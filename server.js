9cbdeb81ae3b14e8d385ff4862722f101d9638d6
Charles Badibanga <charlesbadibanga2000@yahoo.fr>
2021-05-26 12:42:28 -0600

    deploying

--- server.js
+++ server.js
@@ -15,11 +15,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
-
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/workout',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 app.get("/stats", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/stats.html"))
 })
